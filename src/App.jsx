c0ec804e0c23cc9e62d162f47e79e4c6764d4432
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-09-25 21:23:52 +0200

    Updated Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { styled } from "@mui/material";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+
+const Title = styled(Box)(({ theme }) => ({
+  backgroundColor: "aliceblue",
+  boxShadow: "5px 5px 5px lightcyan",
+}));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">This is Damiano Manzillo Portfolio 💼</p>
+      <Title>
+        <p className="read-the-docs">This is Damiano Manzillo Portfolio 💼</p>
+      </Title>
     </>
   );
 }
