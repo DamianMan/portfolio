d2148abfa3730a17034411ecdc0269bc303e4739
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-09-25 21:28:26 +0200

    Updated Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import { styled } from "@mui/material";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 
-const Title = styled(Box)(({ theme }) => ({
+const BoxTitle = styled(Box)(({ theme }) => ({
   backgroundColor: "aliceblue",
-  boxShadow: "5px 5px 5px lightcyan",
+  boxShadow: "5px 5px 5px lime",
+  width: "100%",
+}));
+
+const Title = styled("h1")(({ theme }) => ({
+  color: "darkgrey",
+  fontFamily: "Montserrat san-serif",
+  fontWeight: "light",
 }));
 
 function App() {
   return (
     <>
-      <Title>
-        <p className="read-the-docs">This is Damiano Manzillo Portfolio 💼</p>
-      </Title>
+      <BoxTitle>
+        <Title>This is Damiano Manzillo Portfolio 💼</Title>
+      </BoxTitle>
     </>
   );
 }
